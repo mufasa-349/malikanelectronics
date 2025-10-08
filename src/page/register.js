0678703e9cc0c5bad44f71d4c492438d7c339ec6
Mustafa Bozyel <76789838+mufasa-349@users.noreply.github.com>
2025-10-09 01:32:12 +0300

    Delete src directory

--- src/page/register.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Redirect } from 'react-router-dom'
-
-const Register = () => {
-    // Register sayfasını iletişim sayfasına yönlendir
-    return <Redirect to="/login" />
-}
-
-export default Register
